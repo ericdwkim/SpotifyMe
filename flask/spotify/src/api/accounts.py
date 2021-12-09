3264b5646b38344eb8cdaf6dcee47fabedee6f6f
ericdwkim <ericdwkim.io@gmail.com>
2021-12-09 11:13:51 -0500

    (add): boiler plate for accounts api

---
+++ flask/spotify/src/api/accounts.py
@@ -0,0 +1,8 @@
+import hashlib
+import secrets
+
+
+def scramble(password: str):
+    """Hash and salt the given password"""
+    salt = secrets.token_hex(16)
+    return hashlib.sha512((password + salt).encode('utf-8')).hexdigest()
