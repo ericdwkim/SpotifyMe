315c6f3186ede8fd5b17d4530ec65ac7c16f64f1
ericdwkim <ericdwkim.io@gmail.com>
2021-12-09 12:03:22 -0500

    (testing): create endpoint for artists collection with dummy data

--- flask/spotify/src/api/artists.py
+++ flask/spotify/src/api/artists.py
@@ -3,8 +3,9 @@ from ..models import Artist, db
 
 bp_artists = Blueprint('artists', __name__, url_prefix='/artists')
 
+# read from artsts tale
 
-# GET artist objects; test passes
+
 @bp_artists.route('', methods=['GET'])
 def index():
     artists = Artist.query.all()
@@ -13,12 +14,22 @@ def index():
         result.append(a.serialize())
     return jsonify(result)
 
+# create record to artists table
 
-@bp_artists.route('', methods=['POST'])
+
+@bp_artists.route('/create', methods=['POST'])
 def create():
-    # request payload _MUST_ contain artist_name; recall artist_bio is NULLABLE
     if 'artist_name' not in request.json:
         return abort(400)
+    # try:
+    #     Artist.query.get_or_404(request.json['artist_name'])
+    #     a = Artist(
+    #         artist_name=request.json['artist_name']
+    #     )
+    #     a.insert()
+    #     return jsonify(a.serialize())
+    # except:
+    #     return abort(422)
     Artist.query.get_or_404(request.json['artist_name'])
     a = Artist(
         artist_name=request.json['artist_name']
