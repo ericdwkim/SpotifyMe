47d156b23a40b01c434dbc7ea26c150a20562de1
ericdwkim <ericdwkim.io@gmail.com>
2021-12-09 14:40:34 -0500

    (wip): delete api for artists

--- flask/spotify/src/api/artists.py
+++ flask/spotify/src/api/artists.py
@@ -3,9 +3,12 @@ from ..models import Artist, db
 
 bp_artists = Blueprint('artists', __name__, url_prefix='/artists')
 
-# read from artsts tale
+# test
+# @bp_artists.route('', methods=['GET'])
+# def test():
+#     return jsonify({"msg": "hello rld"})
 
-
+# READ localhost:5000/artists
 @bp_artists.route('', methods=['GET'])
 def index():
     artists = Artist.query.all()
@@ -14,25 +17,68 @@ def index():
         result.append(a.serialize())
     return jsonify(result)
 
-# create record to artists table
+# SHOW localhost:5000/artists/<int:id>
+@bp_artists.route('/<int:id>', methods=['GET'])
+def show(id: int):
+    a = Artist.query.get_or_404(id)
+    return jsonify(a.serialize())
 
-
+# CREATE localhost:5000/artists/create
 @bp_artists.route('/create', methods=['POST'])
 def create():
-    if 'artist_name' not in request.json:
+    if 'artist_name' not in request.json or 'artist_bio' not in request.json:
         return abort(400)
-    # try:
-    #     Artist.query.get_or_404(request.json['artist_name'])
-    #     a = Artist(
-    #         artist_name=request.json['artist_name']
-    #     )
-    #     a.insert()
-    #     return jsonify(a.serialize())
-    # except:
-    #     return abort(422)
-    Artist.query.get_or_404(request.json['artist_name'])
-    a = Artist(
-        artist_name=request.json['artist_name']
-    )
-    a.insert()
-    return jsonify(a.serialize())
+    try:
+        a_name = request.json['artist_name']
+        a_bio = request.json['artist_bio']
+        a = Artist(
+            artist_name=a_name, artist_bio=a_bio
+        )
+        a.insert()
+        return jsonify(a.serialize())
+    except:
+        return abort(422)
+
+# UPDATE localhost:5000/artists/<int:id>/update
+@bp_artists.route('/<int:id>/update', methods=['PATCH'])
+def update(id: int):
+    # ensure payload params exist
+    if 'artist_name' not in request.json or 'artist_bio' not in request.json:
+        return abort(400)
+    # ensure payload params are strings
+    if type(request.json['artist_name'] or request.json['artist_bio']) is not str:
+        return abort (400)
+    try:
+        a_id = Artist.query.get_or_404(id)
+        a_id.artist_name = request.json['artist_name']
+        a_id.artist_bio = request.json['artist_bio']
+        a_name = a_id.artist_name
+        a_bio = a_id.artist_bio
+        a = Artist(
+            artist_name=a_name, artist_bio=a_bio
+        )
+        a.update()
+        return jsonify(a.serialize())
+    except:
+        return abort(422)
+
+# DELETE localhost:5000/artists/<int:id>
+@bp_artists.route('/<int:id>', methods=['DELETE'])
+def delete(id: int):
+    # ensure artist w/ id exists
+    # a_id = Artist.query.get_or_404(id)
+    try:
+        a_id = Artist.query.get_or_404(id)
+        a_id.artist_name = request.json['artist_name']
+        a_id.artist_bio = request.json['artist_bio']
+        a_name = a_id.artist_name
+        a_bio = a_id.artist_bio
+        a = Artist(
+            artist_name=a_name, artist_bio=a_bio
+        )
+        # a_id.delete()
+        db.session.delete(a)
+        db.session.commit()
+        return jsonify(True)
+    except:
+        return abort(422)   
