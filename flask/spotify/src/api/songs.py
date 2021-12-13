1137a7275f9c7c85617d5f122b9fa6912bad967b
ericdwkim <ericdwkim.io@gmail.com>
2021-12-13 17:11:01 -0500

    (finished): songs endpoints with tests passing

--- flask/spotify/src/api/songs.py
+++ flask/spotify/src/api/songs.py
@@ -23,53 +23,59 @@ def show(id: int):
     s_id = Song.query.get_or_404(id)
     return jsonify(s_id.serialize())
 
-# # CREATE localhost:5000/songs/create
+# CREATE localhost:5000/songs/create
 @bp_songs.route('/create', methods=['POST'])
 def create():
     if ('song_title' or 'song_writer' or 'song_producer' or 'song_length' or 'release_date') not in request.json:
         return abort(400)
     try:
-        a_name = request.json['artist_name']
-        a_bio = request.json['artist_bio']
-        a_bio = request.json['artist_bio']
-        a_bio = request.json['artist_bio']
-        a_bio = request.json['artist_bio']
-        a = Song(
-            artist_name=a_name, artist_bio=a_bio
+        s_title = request.json['song_title']
+        s_writer = request.json['song_writer']
+        s_producer = request.json['song_producer']
+        s_length = request.json['song_length']
+        s_releaseDate = request.json['release_date']
+        s = Song(
+            song_title=s_title, song_writer=s_writer, song_producer=s_producer, song_length=s_length, release_date=s_releaseDate
         )
-        a.insert()
-        return jsonify(a.serialize())
+        s.insert()
+        return jsonify(s.serialize())
     except:
         return abort(422)
 
-# # UPDATE localhost:5000/songs/<int:id>/update
-# @bp_songs.route('/<int:id>/update', methods=['PATCH'])
-# def update(id: int):
-#     if ('artist_name' or 'artist_bio') not in request.json:
-#         return abort(400)
-#     if type(request.json['artist_name'] or request.json['artist_bio']) is not str:
-#         return abort (400)
-#     try:
-#         a_id = Song.query.get_or_404(id)
-#         a_id.artist_name = request.json['artist_name']
-#         a_id.artist_bio = request.json['artist_bio']
-#         a_name = a_id.artist_name
-#         a_bio = a_id.artist_bio
-#         a = Song(
-#             artist_name=a_name, artist_bio=a_bio
-#         )
-#         a.update()
-#         return jsonify(a.serialize())
-#     except:
-#         return abort(422)
+# UPDATE localhost:5000/songs/<int:id>/update
+@bp_songs.route('/<int:id>/update', methods=['PATCH'])
+def update(id: int):
+    if ('song_title' or 'song_writer' or 'song_producer' or 'song_length' or 'release_date') not in request.json:
+        return abort(400)
+    if type(request.json['song_title'] or request.json['song_writer'] or request.json['song_producer']) is not str:
+        return abort (400)
+    try:
+        s_id = Song.query.get_or_404(id)
+        s_id.song_title = request.json['song_title']
+        s_id.song_writer = request.json['song_writer']
+        s_id.song_producer = request.json['song_producer']
+        s_id.song_length = request.json['song_length']
+        s_id.releaseDate = request.json['release_date']
+        s_title = s_id.song_title
+        s_writer = s_id.song_writer
+        s_producer = s_id.song_producer
+        s_length = s_id.song_length
+        s_releaseDate = s_id.releaseDate
+        s = Song(
+            song_title=s_title, song_writer=s_writer, song_producer=s_producer, song_length=s_length, release_date=s_releaseDate
+        )
+        s.update()
+        return jsonify(s.serialize())
+    except:
+        return abort(422)
 
 # # DELETE localhost:5000/songs/<int:id>
-# @bp_songs.route('/<int:id>', methods=['DELETE'])
-# def delete(id: int):
-#     a_id = Song.query.get_or_404(id)
-#     try:
-#         db.session.delete(a_id)
-#         db.session.commit()
-#         return jsonify(True)
-#     except:
-#         return jsonify(False)
+@bp_songs.route('/<int:id>', methods=['DELETE'])
+def delete(id: int):
+    s_id = Song.query.get_or_404(id)
+    try:
+        db.session.delete(s_id)
+        db.session.commit()
+        return jsonify(True)
+    except:
+        return jsonify(False)
