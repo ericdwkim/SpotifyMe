94e18a8f289b8ae230a844ea019c7bfc5d893a74
ericdwkim <ericdwkim.io@gmail.com>
2021-12-17 19:11:42 -0500

    (wip): models unit testing

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,9 +1,25 @@
-from ...flask.spotify.src import songs, albums, groups, accounts, artists
+# from ...flask.spotify.src import songs, albums, groups, accounts, artists
+from ...flask.spotify.src.models import Song, Album, Group, Account, Artist
 
 def test_new_song():
     """
-    TODO: implement 
+    GIVEN a Song model
+    WHEN a new Song is created
+    THEN check the title, writer, producer, length, and release_date fields are defined correctly
     """
+    song = Song(
+        'Stan', 
+        'Eminem',
+        'Dr. Dre',
+        6.44,
+        '05-23-2000'
+    )
+
+    assert song.song_title == 'Stan'
+    assert song.song_writer == 'Eminem'
+    assert song.song_producer == 'Dr. Dre'
+    assert song.song_length == 6.44
+    assert song.release_date == '05-23-2000'
 
 def test_new_album():
     """
