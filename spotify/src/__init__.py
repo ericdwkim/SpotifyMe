c347b53a002169c0c0eda6a1d671f0ca41cb9bdb
ericdwkim <ericdwkim.io@gmail.com>
2021-12-30 15:51:34 -0500

    successfully mapped flask, db ports for a 404 response; (wip) accessing endpoints

--- spotify/src/__init__.py
+++ spotify/src/__init__.py
@@ -12,7 +12,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI='postgresql://postgres@localhost:5432/spotify',
+        SQLALCHEMY_DATABASE_URI='postgresql://postgres@localhost:15432/spotify',
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
         SQLALCHEMY_ECHO=True
     )
