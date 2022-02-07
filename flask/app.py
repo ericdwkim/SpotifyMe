c7b84c41991dc3f093e28515d71e2f5ec43877eb
ericdwkim <ericdwkim.io@gmail.com>
2022-02-07 10:41:16 -0500

    local dev setup; installed dotenv lib

--- flask/app.py
+++ flask/app.py
@@ -12,8 +12,8 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        # SQLALCHEMY_DATABASE_URI='postgresql://postgres@localhost:5432/spotify',
-        SQLALCHEMY_DATABASE_URI='postgresql://postgres@pg_spotifyMe:5432/spotify',
+        SQLALCHEMY_DATABASE_URI='postgresql://postgres@localhost:5432/spotify',
+        # SQLALCHEMY_DATABASE_URI='postgresql://postgres@pg_spotifyMe:5432/spotify',
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
         SQLALCHEMY_ECHO=True
     )
