eb294566038c5c5093308788623a5e767f4c2d47
ericdwkim <ericdwkim.io@gmail.com>
2022-02-12 06:49:30 -0500

    file --> track; second try block implementation is completed but not tested due to inherent inefficiency, will come back to later

--- flask/src/seed_s3.py
+++ flask/src/seed_s3.py
@@ -17,26 +17,34 @@ bucket_name = os.getenv('bucket_name')
 def main():
 
     # Connect to S3 Service
-    s3 = boto3.client(
+    s3_client = boto3.client(
         's3',
         aws_access_key_id=access_key,
         aws_secret_access_key=access_secret
     )
 
+    # Connect to S3 Resource
+    # s3_rsrc = boto3.resource(
+    #     's3',
+    #     aws_access_key_id=access_key,
+    #     aws_secret_access_key=access_secret
+    # )
+    # bucket =  s3_rsrc.Bucket(bucket_name)
+
     # Upload track files to S3 &&  grant READ only permissions to objects
     data_file_path = os.path.join(os.getcwd(), 'audio_tracks/upload')
-    for file in os.listdir(data_file_path):
-        if not file.startswith('~'):
+    for track in os.listdir(data_file_path):
+        if not track.startswith('~'):
             try:
-                print('Uploading file:  {0}...'.format(file))
-                s3.upload_file(
-                    os.path.join(data_file_path, file),
+                print('Uploading track:  {0}...'.format(track))
+                s3_client.upload_file(
+                    os.path.join(data_file_path, track),
                     bucket_name,
-                    file,
+                    track,
                     ExtraArgs={
                         'ACL':'public-read'}
                 )
-
+            
             except ClientError as e:
                 logging.error(e)
                 return False
@@ -52,13 +60,21 @@ if __name__ == '__main__':
 TODO: implement as new try block; lists all current s3 objects and last date modified
             need resource or client obj sufficient? 
 
+            implementation is below; however nested for loop is inefficient; perhaps append 
+            newly uploaded track to empty array and print array at the end using only 1 for loop?
+
 s3_rsrc = boto3.resource('s3')
 bucket =  s3.Bucket(bucket_name)
 
 for track in bucket.objects.all():
     print(obj.key, obj.last_modified) 
 
-    @dev: where obj.key = track title 
-    @dev: search up other obj.<apis> for funsies too
+            # try:
+            #     for track in bucket.objects.all():
+            #         print(track.key, track.last_modified)
+                    
+            # except ClientError as e:
+            #     logging.error(e)
+            #     return False
 
 """
