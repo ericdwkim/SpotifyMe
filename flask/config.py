4e1c4ca0d990c2f81691f61a31414a5893268fa3
ericdwkim <ericdwkim.io@gmail.com>
2022-02-16 20:22:16 -0500

    switching from localhost -> db container to rebuild docker image

--- flask/config.py
+++ flask/config.py
@@ -2,8 +2,8 @@ import psycopg2
 # import pandas
 
 conn = psycopg2.connect(
-    # host = 'pg_spotifyMe',
-    host = 'localhost',
+    host = 'pg_spotifyMe',
+    # host = 'localhost',
     database = 'spotify',
     user = 'postgres',
     port = '5432'
